860816a0e3dcacfead8912caa48fbf71d30426c8
ChoiYuTat <230131555@stu.vtc.edu.hk>
2025-01-06 17:30:30 +0800

    SaveUI

---
+++ ITE4116M/Assets/scripts 1/UI/RecordDataUI.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class RecordDataUI : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    public Text indexText;
+    public Text recordName;
+    public GameObject auto;
+    public Image rect;
+    [ColorUsage(true)] 
+    public Color enterColor;
+
+    public static System.Action<int> OnLeftClick;
+    public static System.Action<int> OnRightClick;
+    public static System.Action<int> OnEnter;
+    public static System.Action<int> OnExit;
+
+    int id;
+
+    private void Start()
+    {
+        id = transform.GetSiblingIndex();
+    }
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button==PointerEventData.InputButton.Left)
+        {
+            if(OnLeftClick != null)
+            {
+                OnLeftClick(id);
+            }
+        }
+
+        if (eventData.button == PointerEventData.InputButton.Right)
+        {
+            if(OnRightClick != null)
+            { 
+                OnRightClick(id); 
+            }
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        rect.color = enterColor;
+
+        if(recordName.text !="Empty")
+        {
+            if(OnEnter != null)
+            {
+                OnEnter(id);
+            }
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        rect.color = Color.white;
+
+        if(OnExit != null)
+        {
+            //OnExit();
+        }
+    }
+
+    public void SetID(int i)
+    {
+        indexText.text = i.ToString();
+    }
+
+    public void SetName(int i)
+    {
+        if (RecordData.Instance.recordName[i]=="")
+        {
+            recordName.text = "Empty";
+            auto.SetActive(false);
+
+        }
+        else
+        {
+            string full = RecordData.Instance.recordName[i];
+
+            string date = full.Substring(0, 8);
+
+            string time = full.Substring(9, 6);
+
+            //TimeManager.SetDate(ref date);
+            //TimeManager.SetTime(ref time);
+
+            recordName.text = date+" "+time;
+
+            if(full.Substring(full.Length -4)=="auto")
+            {
+                auto.SetActive(true);
+            }
+            else
+            {
+                auto.SetActive(false) ;
+            }
+        }
+    }
+
+}
