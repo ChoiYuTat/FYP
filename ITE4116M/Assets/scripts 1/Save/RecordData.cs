ba2d12b5c1f3abba3855df2aea8d098e482fe7bb
ChoiYuTat <230131555@stu.vtc.edu.hk>
2025-01-06 15:59:40 +0800

    RecordData

--- ITE4116M/Assets/scripts 1/Save/RecordData.cs
+++ ITE4116M/Assets/scripts 1/Save/RecordData.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class RecordData : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public const int recordNum = 3;
+    public const string NAME = "RecordData";
+
+    public string[] recordName = new string[recordNum];
+    public int lastID;
+
+    class SaveData
+    {
+        public string[] recordName = new string[recordNum];
+        public int lastID;
+    }
+
+   SaveData ForSave()
+    {
+        var saveData = new SaveData();
+
+        for(int i = 0; i < recordNum; i++)
+        {
+            saveData.recordName[i] = recordName[i];
+        }
+        saveData.lastID = lastID;
+
+        return saveData;
+    }
+    void ForLoad(SaveData saveData)
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    public void Save()
     {
-        
+        SaveSystem.SaveByPlayerPrefs(NAME,ForSave());
+    }
+
+    public void Load()
+    {
+        if(PlayerPrefs.HasKey(NAME))
+        {
+            string json = SaveSystem.LoadFromPlayerPrefs(NAME);
+            SaveData saveData = JsonUtility.FromJson<SaveData>(json);
+            ForLoad(saveData);
+        }
     }
 }
--- ITE4116M/Assets/scripts 1/Save/SaveSystem.cs
+++ ITE4116M/Assets/scripts 1/Save/SaveSystem.cs
@@ -5,10 +5,12 @@ using System.IO;
 
 public class SaveSystem
 {
+    
     static string GetPath(string fileName)
     {
         return Path.Combine(Application.persistentDataPath, fileName);
     }
+    #region PlayerPrefs
     public static void SaveByPlayerPrefs(string key, object data)
     {
         string json = JsonUtility.ToJson(data);
@@ -19,13 +21,40 @@ public class SaveSystem
     {
         return PlayerPrefs.GetString(key, null);
     }
-
+    #endregion
+    #region JSON
     public static void JSONSave(string fileName, object data)
     {
         string json = JsonUtility.ToJson((object)data);
         File.WriteAllText(GetPath(fileName), json);
- 
+#if UNITY_EDITOR
         Debug.Log($"Save{GetPath(fileName)}");
-
+#endif
     }
+
+    public static T JSONLoad<T>(string fileName)
+    {
+        string path = GetPath(fileName);
+
+        if(File.Exists(path))
+        {
+            string json= File.ReadAllText(GetPath(fileName));
+            var data = JsonUtility.FromJson<T>(json);
+            Debug.Log($"Load{path}");
+            return data;
+        }
+        else
+        {
+            return default;
+        }
+    }
+
+    public static void JSONDelete(string fileName) 
+    {
+        File.Delete(GetPath(fileName));
+    }
+    
+    //public static string FindAuto()
+    #endregion
+
 }
