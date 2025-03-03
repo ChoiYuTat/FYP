0ac06a73f93cc950924a9de23261d94aebe937f7
ChoiYuTat <230131555@stu.vtc.edu.hk>
2025-03-03 16:54:36 +0800

    Update FogController.cs

--- ITE4116M/Assets/scripts 1/FogController.cs
+++ ITE4116M/Assets/scripts 1/FogController.cs
@@ -7,21 +7,40 @@ public class FogController : MonoBehaviour
 {
 
     public Button fogButton;
-    private bool isFogEnabled = true;
+    public float fadeDuration = 3.0f;
+    private bool isFading = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        fogButton.onClick.AddListener(ToggleFog);    
+        fogButton.onClick.AddListener(StatFadeFog);    
     }
 
-
-    void ToggleFog()
+    void StatFadeFog()
     {
-        isFogEnabled = !isFogEnabled;
-        RenderSettings.fog = isFogEnabled;
-
+        if(!isFading)
+        {
+            StartCoroutine(FadeFogDensity());
+        }
     }
+
+    System.Collections.IEnumerator FadeFogDensity()
+    {
+        isFading = true;
+        float startDensity = RenderSettings.fogDensity;
+        float targetDensity = 0.0f;
+        float elapsedTime = 0f;
+        while (elapsedTime<fadeDuration)
+        {
+            RenderSettings.fogDensity = Mathf.Lerp(startDensity, targetDensity, elapsedTime / fadeDuration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        RenderSettings.fogDensity = targetDensity;
+        RenderSettings.fog = false;
+        isFading = false;
+    }
+
     // Update is called once per frame
     void Update()
     {
