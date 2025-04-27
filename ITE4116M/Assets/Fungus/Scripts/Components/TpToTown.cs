ce624e845caf3dfbcc1a5bec0707897b74eaf0fd
LeungOnTik <161811533+LeungOnTik@users.noreply.github.com>
2025-04-27 23:14:31 +0800

    fix

--- ITE4116M/Assets/Fungus/Scripts/Components/TpToTown.cs
+++ ITE4116M/Assets/Fungus/Scripts/Components/TpToTown.cs
@@ -11,7 +11,7 @@ public class TpToTwon : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            Debug.Log(123);
+           
 
             SceneManager.LoadScene(3);
         }
