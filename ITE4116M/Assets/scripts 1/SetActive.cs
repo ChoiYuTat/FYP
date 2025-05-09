91cd0f082f486c55d23179d274fe0917dacdf4d7
ChoiYuTat <230131555@stu.vtc.edu.hk>
2025-05-10 02:01:27 +0800

    gg

--- ITE4116M/Assets/scripts 1/SetActive.cs
+++ ITE4116M/Assets/scripts 1/SetActive.cs
@@ -1,26 +1,60 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SetActive : MonoBehaviour
 {
+    public List <Button> button;
+
     public GameObject option;
     public GameObject userguide;
+    public GameObject image;
     // Start is called before the first frame update
     void Start()
     {
-       
+        foreach (Button btn in button)
+        {
+            Button currentBtn = btn; 
+            currentBtn.onClick.AddListener(() => OnButtonClicked(currentBtn));
+        }
+
+        void OnButtonClicked(Button clickedBtn)
+        {
+            foreach (Button btn in button)
+            {
+                
+                btn.interactable = (btn == clickedBtn);
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-       
 
+
+        
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            option.SetActive(false);
-            userguide.SetActive(false);
+            Enabled();
+            foreach (Button btn in button)
+            {
+                btn.interactable = true;
+            }
         }
+    }
+
+    private void Enabled()
+    {
+        option.SetActive(false);
+        userguide.SetActive(false);
+        image.SetActive(true);
+    }
+
+   public void Exit()
+    {
+        Application.Quit();
     }
 }
