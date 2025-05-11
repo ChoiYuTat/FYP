c105c2b703254082a6001bcf1beaa075427cca3f
LeungOnTik <161811533+LeungOnTik@users.noreply.github.com>
2025-05-11 16:33:16 +0800

    lose update

--- ITE4116M/Assets/scripts 1/Fight/Fight_Loss.cs
+++ ITE4116M/Assets/scripts 1/Fight/Fight_Loss.cs
@@ -1,6 +1,8 @@
+using Fungus;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Fight_Loss : FightUnit
 {
@@ -10,6 +12,29 @@ public class Fight_Loss : FightUnit
         {
             FightManager.Instance.isfirst = true;
             FightManager.Instance.StopAllCoroutines();
+            FightUIManager.Instance.CloseAllUI("FightUI");
+
+            SceneManager.LoadScene("HD_2D_Day");
+
+            GameObject flowchartObj = GameObject.Find("Flowchart(windy)");
+            if (flowchartObj == null)
+            {
+                Debug.LogError("找不到 Flowchart(windy) 對象！");
+                return;
+            }
+            Flowchart flowchart = flowchartObj.GetComponent<Flowchart>();
+            if (flowchart == null)
+            {
+                Debug.LogError("Flowchart 組件未找到！");
+                return;
+            }
+            BooleanVariable gameLoseVar = flowchart.GetVariable<BooleanVariable>("GameLose");
+            gameLoseVar.Value = true;
+            BooleanVariable gameEndVar = flowchart.GetVariable<BooleanVariable>("GameEnd");
+            gameEndVar.Value = true;
+            DungeonGenerator.Instance.ReloadDungeon();
+
+            
         });
     }
 
