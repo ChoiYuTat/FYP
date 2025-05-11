472004c08ecb6c651f15b9b80cdbe516770e6abf
LeungOnTik <161811533+LeungOnTik@users.noreply.github.com>
2025-05-11 17:45:26 +0800

    test

---
+++ ITE4116M/Assets/exit.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class exit : MonoBehaviour
+{
+    public GameObject exitGameObject; 
+    private void Awake()
+    {
+        exitGameObject = GameObject.Find("DungeonManager");
+    }
+
+    public void destory()
+    {
+        Destroy(exitGameObject);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
